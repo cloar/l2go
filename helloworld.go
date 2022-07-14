84866834169aed574af4ac99f7f832da29175202
Christian Loar <christian.loar@coinbase.com>
2022-07-14 11:27:20 -0700

    my first hello world

---
+++ helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World.")
+	foo()
+	fmt.Println("This is after the foo function line.")
+}
+
+func foo() {
+	fmt.Println("I'm hungry for lunch")
+}
+
+// control flow
+// 1. sequence
+// 2. loop: iterative
+// 3. conditional
