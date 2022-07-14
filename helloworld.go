a7e1f8b0ce1edbbe8e4847331b5b394eddcf15a1
Christian Loar <christian.loar@coinbase.com>
2022-07-14 12:37:45 -0700

    Im learning

--- helloworld.go
+++ helloworld.go
@@ -6,13 +6,22 @@ func main() {
 	fmt.Println("Hello World.")
 	foo()
 	fmt.Println("This is after the foo function line.")
+
+	tester := "42"
+
+	fmt.Println(tester)
 }
 
 func foo() {
-	fmt.Println("I'm hungry for lunch")
+	n, _ := fmt.Println("I'm hungry for lunch so let's eat! NOW!!!")
+	fmt.Println(n)
+
 }
 
 // control flow
 // 1. sequence
 // 2. loop: iterative
 // 3. conditional
+
+// you can't have unused variables in Go.  This is CODE POLLUTION
+//
